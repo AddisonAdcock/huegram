8a60fa2c8dac31f60e867fec6aa714da2176aee2
AddisonAdcock <addison.adcock@students.fhu.edu>
2023-12-02 14:13:59 -0600

    Fetching from the Backend Works Well

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,40 @@
-//import Menu from './components/Menu'
-import Main from './components/Main'
-import Profile from './components/Profile'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Main from './components/Main';
+import Profile from './components/Profile';
 
 function App() {
-
   const [hues, setHues] = useState([]);
 
   const [currentUser] = useState({
-    username: "kavery",
+    username: 'kavery',
     likes: 58,
-    hues: [ {id:36, color:'#ffa510', username:"kavery", likes: 15}]
+    hues: [{ id: 36, color: '#ffa510', username: 'kavery', likes: 15 }],
   });
 
-
-  useEffect( ()=>
-  {
+  useEffect(() => {
     fetch('https://greenegunnar.pythonanywhere.com/api/hues/')
-    .then( res => res.json() )
-    .then( data => setHues(data) ) 
-  }, [])
+      .then((res) => res.json())
+      .then((data) =>
+        setHues(data.map((item) => ({ ...item, color: item.hex_code })))
+      );
+  }, []);
 
-  const addNewHue = (color:string ) => 
-  {
-      console.log(color)
-      const newHue = {color, username: currentUser.username, id: length+1 , likes:0};
-      setHues( [newHue, ...hues ] );
-  }
+  const addNewHue = (color: string) => {
+    const newHue = {
+      color,
+      username: currentUser.username,
+      id: hues.length + 1,
+      likes: 0,
+    };
+    setHues([newHue, ...hues]);
+  };
 
   return (
-    <div className='flex bg-slate-800 h-screen'>
-      {/* <Menu /> */}
-
-      <Main hues={hues} addHue = {addNewHue} />
-
+    <div className="flex bg-slate-800 h-screen">
+      <Main hues={hues} addHue={addNewHue} />
       <Profile />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
