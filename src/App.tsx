8fe289ce3c5aca651a6d619fba04cd39157b53c7
AddisonAdcock <addison.adcock@students.fhu.edu>
2023-12-08 19:34:22 -0600

    Progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Main from './components/Main';
 import Profile from './components/Profile';
 
 interface HueObject {
-  id?: number,
-  color: string,
-  username: string,
-  likes: number,
-  isLiked: boolean
+  id?: number;
+  color: string;
+  username: string;
+  likes: number;
+  isLiked: boolean;
 }
 
 interface User {
@@ -18,46 +18,85 @@ interface User {
 
 function App() {
   const [hues, setHues] = useState<HueObject[]>([]);
-
-  const [currentUser] = useState<User>({
-    username: 'kavery',
-    likes: 58,
-    hues: [{ id: 36, color: '#ffa510', username: 'kavery', likes: 15, isLiked: false }],
-  });
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   useEffect(() => {
+    fetchUserData().then((data) => {
+      if (data) {
+        setCurrentUser(data);
+      }
+    });
+
+    fetchHuesData();
+  }, []);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch('/userData.json');
+      const userData = await response.json();
+      return userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      return null;
+    }
+  };
+
+  const fetchHuesData = () => {
     fetch('https://greenegunnar.pythonanywhere.com/api/hues/')
       .then((res) => res.json())
       .then((data) =>
-        setHues(data.map((item: { hex_code: string }) => ({ ...item, color: item.hex_code, isLiked: false })))
+        setHues(
+          data.map((item: { hex_code: string }) => ({
+            ...item,
+            color: item.hex_code,
+            isLiked: false,
+          }))
+        )
       );
-  }, []);
+  };
 
   const addNewHue = (color: string) => {
     const newHue: HueObject = {
       color,
-      username: currentUser.username,
+      username: currentUser?.username || 'default',
       id: hues.length + 1,
       likes: 0,
       isLiked: false,
     };
+
     setHues([newHue, ...hues]);
+
+    // Update user data
+    setCurrentUser((prevUser) => ({
+      ...prevUser!,
+      hues: [newHue, ...prevUser!.hues],
+    }));
   };
 
   const toggleLikeForHue = (id: number) => {
-    const newHues =[...hues]
-    const hue = newHues.find((hue) => hue.id === id)
-    if (hue){
-      hue.isLiked = !hue.isLiked
-      setHues(newHues)
-    }
-   
-  }
+    setHues((prevHues) =>
+      prevHues.map((hue) =>
+        hue.id === id
+          ? { ...hue, isLiked: !hue.isLiked, likes: hue.likes + (hue.isLiked ? -1 : 1) }
+          : hue
+      )
+    );
+
+    // Update user data
+    setCurrentUser((prevUser) => ({
+      ...prevUser!,
+      hues: prevUser!.hues.map((hue) =>
+        hue.id === id
+          ? { ...hue, isLiked: !hue.isLiked, likes: hue.likes + (hue.isLiked ? -1 : 1) }
+          : hue
+      ),
+    }));
+  };
 
   return (
     <div className="flex bg-slate-800 h-screen">
       <Main hues={hues} addHue={addNewHue} toggleLike={toggleLikeForHue} />
-      <Profile />
+      <Profile currentUser={currentUser} />
     </div>
   );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,52 +1,39 @@
-import './Palette.css'
+//import React from 'react';
 
-const Profile = () => {
-  return (
-    <div className='flex flex-col border-2 p-8 items-center text-white'>
-
-        
-
-        <div className="palette">
-          <div style={{ backgroundColor: '#ff2299'}}></div>
-          <div style={{ backgroundColor: '#ee2299'}}></div>
-          <div style={{ backgroundColor: '#dd2299'}}></div>
-          <div style={{ backgroundColor: '#cc2299'}}></div>
-          <div style={{ backgroundColor: '#bb2299'}}></div>
-          
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div ></div>
-
-        </div>
-
-        <h1>@kodom</h1>
-        {/* <div className="flex w-1/2">
-            <img src={kaylee} alt="" className='rounded-full'/>
-        </div> */}
-
-    </div>
-  )
+interface HueObject {
+  id?: number;
+  color: string;
+  username: string;
+  likes: number;
+  isLiked: boolean;
 }
 
-export default Profile
+interface User {
+  username: string;
+  likes: number;
+  hues: HueObject[];
+}
+
+const Profile = ({ currentUser }: { currentUser: User | null }) => {
+  if (!currentUser) {
+    return null; // or display a loading state
+  }
+
+  return (
+    <div className="flex flex-col border-2 p-8 items-center text-white">
+      <div className="palette">
+        {currentUser.hues.map((hue) => (
+          <div key={hue.id} style={{ backgroundColor: hue.color }}></div>
+        ))}
+      </div>
+
+      <h1>@{currentUser.username}</h1>
+      <p>Likes: {currentUser.likes}</p>
+      <p>Posts: {currentUser.hues.length}</p>
+
+      {/* Other user stats can be displayed here */}
+    </div>
+  );
+};
+
+export default Profile;
