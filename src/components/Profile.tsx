18f0c789a750479c00f0cfb644596f0294b0d4d8
AddisonAdcock <addison.adcock@students.fhu.edu>
2023-12-09 14:50:22 -0600

    Remove unused code in Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -21,17 +21,9 @@ const Profile = ({ currentUser }: { currentUser: User | null }) => {
 
   return (
     <div className="flex flex-col border-2 p-8 items-center text-white">
-      <div className="palette">
-        {currentUser.hues.map((hue) => (
-          <div key={hue.id} style={{ backgroundColor: hue.color }}></div>
-        ))}
-      </div>
-
       <h1>@{currentUser.username}</h1>
       <p>Likes: {currentUser.likes}</p>
       <p>Posts: {currentUser.hues.length}</p>
-
-      {/* Other user stats can be displayed here */}
     </div>
   );
 };
