595a884ffc22eec7f61ab23f83cfc0805b7d320c
AddisonAdcock <addison.adcock@students.fhu.edu>
2023-12-09 16:38:21 -0600

    Profile Works

--- src/App.tsx
+++ src/App.tsx
@@ -148,4 +148,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,5 @@
-//import React from 'react';
-import './Palette.css'
+import React from 'react';
+import './Palette.css';
 
 interface HueObject {
   id?: number;
@@ -20,41 +20,25 @@ const Profile = ({ currentUser }: { currentUser: User | null }) => {
     return null; // or display a loading state
   }
 
+  // Create an array of color codes from the user's hues
+  const paletteColors = currentUser.hues.map((hue) => hue.color);
+
+  // Create an array with 25 elements, filling extra spaces with empty strings
+  const emptyColors = Array(25 - paletteColors.length).fill('');
+
   return (
     <div className="flex flex-col border-2 p-8 items-center text-white">
-      
       <h1>@{currentUser.username}</h1>
       <div className="palette pb-5 pt-5">
-          <div style={{ backgroundColor: '#ff2299'}}></div>
-          <div style={{ backgroundColor: '#ee2299'}}></div>
-          <div style={{ backgroundColor: '#dd2299'}}></div>
-          <div style={{ backgroundColor: '#cc2299'}}></div>
-          <div style={{ backgroundColor: '#bb2299'}}></div>
-          
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-          <div style={{ backgroundColor: ''}}></div>
-        </div>
+        {/* Map over the user's hues and create corresponding divs */}
+        {paletteColors.map((color, index) => (
+          <div key={index} style={{ backgroundColor: color }}></div>
+        ))}
+        {/* Fill remaining spaces with empty divs */}
+        {emptyColors.map((_, index) => (
+          <div key={index}></div>
+        ))}
+      </div>
       <h1>{currentUser.likes}</h1>
       <p>Likes</p>
       <h1>{currentUser.hues.length}</h1>
